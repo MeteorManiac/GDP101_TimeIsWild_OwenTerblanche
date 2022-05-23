93906180db05fba0da770b0e6106f79815df995a
MeteorManiac <104057612+MeteorManiac@users.noreply.github.com>
2022-05-23 19:20:41 +0200

    Enemy turret Wip

---
+++ Timed Attack!!/Assets/DetectionFOV.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionFOV : MonoBehaviour
+{
+    public float radius;
+    public float angle;
+
+    public GameObject playerRef;
+
+    public LayerMask targetMask;
+    public LayerMask targetMask;
+}
--- Timed Attack!!/Assets/Script/EnemyTurret.cs
+++ Timed Attack!!/Assets/Script/EnemyTurret.cs
@@ -10,14 +10,18 @@ public class EnemyTurret : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       Vector3 direction = playerTank.position - transform.position;
-       float angle = Mathf.Atan2(direction.y, direction.x)* Mathf.Rad2Deg-90;
-       rb.rotation = angle;
+        if(playerTank)
+        {
+            Vector3 direction = playerTank.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+            rb.rotation = angle;
+        }
     }
 }
