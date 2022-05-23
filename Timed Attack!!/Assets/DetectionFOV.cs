caf1bea86865f911cea25c925ccc63d12024d9d1
MeteorManiac <104057612+MeteorManiac@users.noreply.github.com>
2022-05-23 19:48:39 +0200

    Enemy FOV WIP
    FOV used by enemy to find and shoot the player

--- Timed Attack!!/Assets/DetectionFOV.cs
+++ Timed Attack!!/Assets/DetectionFOV.cs
@@ -10,5 +10,50 @@ public class DetectionFOV : MonoBehaviour
     public GameObject playerRef;
 
     public LayerMask targetMask;
-    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    public bool canSeePlayer;
+
+    private void Start()
+    {
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVRoutine());
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        float delay = 0.2f;
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            fieldOfViewCheck();
+        }
+    }
+
+    private void fieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere (transform.position, radius, targetMask);
+
+        if(rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if(Vector3.Angle(transform.forward, directionToTarget) <angle / 2)
+            {
+                float distanceToTaget = Vector3.Distance(transform.position, target.position);
+
+                if(!Physics.Raycast(transform.position, directionToTarget,distanceToTaget, obstructionMask))
+                    canSeePlayer = true;
+                else 
+                    canSeePlayer = false;
+            }
+            else
+                canSeePlayer = false;
+        }
+        else if(canSeePlayer)
+            canSeePlayer = false;
+    }
 }
